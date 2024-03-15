a1cae43bb59ecba41da8cde96cc32aeb45f66d8a
Your Name <gfalbery@gmail.com>
2024-03-15 13:50:07 -0400

    Initial push

---
+++ X_Friterature Review.R
@@ -0,0 +1,117 @@
+
+# X_Friterature Review
+
+library(gsheet); library(magrittr); library(tidyverse)
+
+LitReview <- gsheet2tbl("https://docs.google.com/spreadsheets/d/15XXz_ZfBm-qEc39RDxQvJDyQ2iJ8xXSiMhK6hIijwK0/edit?usp=sharing")
+
+LitReview %<>% as.data.frame
+
+LitReview %<>% rename_all(~.x %>% str_trim %>% str_replace_all(" ", "_"))
+
+LitReview %>% head
+
+LitReview %>% pull(YNM) %>% table
+
+LitReview %>% pull(YNM) %>% table %>% prop.table
+
+LitReview %>% filter(YNM == "Y")
+
+LitReview %>% filter(YNM == "M")
+
+library(cowplot)
+
+theme_set(theme_cowplot())
+
+# Exploring changes through time ####
+
+LitReview %>% 
+  filter(YNM == "Y") %>% 
+  count(Publication_Year) %>% 
+  ggplot(aes(Publication_Year, n)) +
+  geom_point()
+
+LitReview %>% 
+  filter(YNM == "Y") %>% 
+  count(Publication_Year) %>% mutate_at("n", cumsum) %>% 
+  ggplot(aes(Publication_Year, n)) +
+  geom_point()
+
+LitReview %>% 
+  filter(YNM == "Y") %>% 
+  count(Publication_Year) %>% mutate_at("n", cumsum) %>% 
+  ggplot(aes(Publication_Year, n)) +
+  geom_line()
+
+LitReview %>% 
+  filter(YNM == "M") %>% 
+  count(Publication_Year) %>% mutate_at("n", cumsum) %>% 
+  ggplot(aes(Publication_Year, n)) +
+  geom_line()
+
+LitReview %>% 
+  filter(YNM == "N") %>% 
+  count(Publication_Year) %>% mutate_at("n", cumsum) %>% 
+  ggplot(aes(Publication_Year, n)) +
+  geom_line()
+
+LitReview %>% 
+  # filter(YNM == "N") %>% 
+  group_by(YNM) %>%
+  count(Publication_Year) %>% mutate_at("n", cumsum) %>% 
+  ggplot(aes(Publication_Year, n, colour = YNM)) +
+  geom_line()
+
+LitReview %>% 
+  # filter(YNM == "N") %>% 
+  group_by(YNM) %>% 
+  count(Publication_Year) %>% mutate_at("n", cumsum) %>% 
+  mutate_at("n", ~scales::rescale(.x, c(0, 1))) %>% 
+  ggplot(aes(Publication_Year, n, colour = YNM)) +
+  geom_line()
+
+# Saving DOIs to save time ####
+
+LitReview$DOI %>% is.na %>% table
+
+LitReview$DOI %>% table %>% table
+
+LitReview %>% 
+  dplyr::select(Article_Title, DOI) %>% 
+  is.na %>% colSums()
+
+LitReview$Article_Title %>% table %>% table
+
+Done <- 
+  LitReview %>% #slice(1:100) %>% 
+  dplyr::select(Article_Title, DOI)
+
+LitReview2 <- #LitReview
+  gsheet2tbl("AgeingNewLitReview.html or whatever")
+
+LitReview2 %>% 
+  # anti_join(Done, by = c("DOI")) %>%
+  nrow
+
+# Looking at some abstract sifting ####
+
+Maybes <- LitReview %>% mutate(Row = 1:n()) %>% 
+  filter(YNM == "M")
+
+Maybes %>% 
+  filter(str_detect(Abstract, "Mammal|mammal"))
+
+Maybes %>% 
+  filter(str_detect(Abstract, "nfect"))
+
+Maybes$Abstract %>% str_count("nfect") %>% sum(na.rm = T)
+
+Maybes$Abstract %>% str_count("athogen") %>% sum(na.rm = T)
+
+Maybes %>% 
+  mutate(Output = paste0(Row, ": ", Authors, ": ", Article_Title, ": ", DOI, "\nAbstract:", Abstract)) %>% 
+  # pull(Output) %>% 
+  # paste(sep = "\n\n") %>% 
+  dplyr::select(Output) %>% 
+  write_delim("Abstracts.txt")
+
